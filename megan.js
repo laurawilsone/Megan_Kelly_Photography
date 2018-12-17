221252d6528051fd0f17473583e81e90a6546801
laurawilsone <laura.wils204@gmail.com>
2018-12-16 20:24:15 -0600

    working on modal

--- megan.js
+++ megan.js
@@ -1,9 +1,3 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "nav-item") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
